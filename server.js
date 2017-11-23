fa48d34d8440f4abf0ee347082ef44e220d80cdd
k0xxx <androidk0xxx@gmail.com>
2017-11-23 18:06:09 +0200

    update

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, '/src')))
+
+app.use(bodyParser.json());                        
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/*', function(req, res){
+	res.sendFile(path.join(__dirname, './src/index.html'));
+})
+
+var server = http.createServer(app);
+
+// Старт сервера на порту
+server.listen(3900, function(){
+	console.log('Server running on port: 3900');
+});
